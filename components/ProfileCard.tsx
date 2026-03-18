2724e95849e0c0996e4048013e1ad5062c8417f7
fneel <70952301+fneel@users.noreply.github.com>
2026-03-18 13:16:19 +0100

    Updated profile card with new props

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -10,13 +10,13 @@ export default function ProfileCard({ student }: ProfileCardProps) {
   return (
     <div className="grid gap-4 border border-white/20 text-neutral-400 p-12 hover:bg-white/10">
       <div className="flex items-center justify-between">
-        {student.image ? (
+        {student.avatarBg ? (
           <Image
             className="border border-white/20 rounded-full w-15 h-15"
             alt=""
             height={50}
             width={50}
-            src={student.image}
+            src={student.avatarBg}
           />
         ) : (
           <div className="border border-white/20 rounded-full w-15 h-15 flex items-center justify-center">
@@ -28,10 +28,10 @@ export default function ProfileCard({ student }: ProfileCardProps) {
 
       <div>
         <h3 className="text-2xl text-white">{student.name}</h3>
-        <span className="text-sm tracking-wide">{student.title}</span>
+        <span className="text-sm tracking-wide">{student.role}</span>
       </div>
 
-      <p className="line-clamp-3 truncate">{student.shortBio}</p>
+      <p className="line-clamp-3 truncate">{student.bio}</p>
 
       <div className="flex flex-wrap gap-2">
         {student.competenceTags.map((tag) => (
@@ -45,18 +45,18 @@ export default function ProfileCard({ student }: ProfileCardProps) {
       </div>
 
       <div className="border-t border-neutral-800 flex gap-2 pt-6 font-mono text-xs tracking-wide">
-        {student.links.linkedin && (
+        {student.linkedIn && (
           <Link
-            href={student.links.linkedin}
+            href={student.linkedIn}
             className="border border-neutral-700 text-neutral-400 rounded-sm text-center content-center px-4 py-2 hover:text-neutral-200 hover:border-neutral-400"
           >
             LinkedIn ↗
           </Link>
         )}
 
-        {student.links.portfolio && (
+        {student.portfolio && (
           <Link
-            href={student.links.portfolio}
+            href={student.portfolio}
             className="border border-neutral-700 text-neutral-950 rounded-sm text-center content-center px-4 py-2  bg-lime-300/70"
           >
             Portfolio →
